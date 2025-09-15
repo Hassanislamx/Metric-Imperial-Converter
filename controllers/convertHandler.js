4698bce6d40e417c6c40e5500de424832931fad2
Hassan Islam <138744113+Hassanislamx@users.noreply.github.com>
2025-09-15 09:26:33 +0000

    commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,13 +2,21 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     let result;
-    
+    let num = input.match(/[.\d\/]+/g) || ["1"];
+    let nums = num[0].split('/');
+      if(nums.length > 2) return 'invalid number';
+    result = nums[0] / nums[1] || nums[0];
+    result = Math.round(result * 100000) / 100000;
+    if(isNaN(result)) return 'invalid number';
     return result;
   };
   
   this.getUnit = function(input) {
     let result;
-    
+    let unit = input.match(/[a-zA-Z]+/)[0];
+    let validUnits = ['gal','l','mi','km','lbs','kg','GAL','L','MI','KM','LBS','KG'];  
+            if(!validUnits.includes(unit)) return 'invalid unit';
+            return unit;
     return result;
   };
   
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,66 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+  suite('Function convertHandler.getNum(input)', function()  
+        {
+          test('Whole number input', function(done){
+                let input = '32L';
+                assert.equal(convertHandler.getNum(input),32);
+                done();
+              
+              });
+            test('Decimal Input', function(done)    
+                {
+                   let input = '3.2L';
+                   assert.equal(convertHandler.getNum(input),3.2);
+                   done();
+                });
+            test('Fractional Input', function(done)
+                {
+                let input = '3/2L';
+                assert.equal(convertHandler.getNum(input),1.5);
+                done();
+              });
+            test('Fractional Input w/ Decimal', function(done)   
+                {
+                let input = '3.2/2L';
+                assert.equal(convertHandler.getNum(input),1.6);
+                done();
+              });
+            test('Invalid Input (double fraction)', function(done)    
+                {
+                let input = '3/2/2L';
+                assert.equal(convertHandler.getNum(input),'invalid number');
+                done();
+              });
+            test('No Numerical Input', function(done){
+                let input = 'L';
+                assert.equal(convertHandler.getNum(input),1);
+                done();
+            });
+            test('Invalid Input (no number)', function(done){
+                let input = 'L';
+                assert.equal(convertHandler.getNum(input),1);
+                done();
+            });
+            
+            }
+        
+        );
+  suite('Function convertHandler.getUnit(input)', function(){
+      test('For Each Valid Unit Inputs', function(done){
+        let input = ['gal','l','mi','km','lbs','kg','GAL','L','MI','KM','LBS','KG'];
+        input.forEach(function(ele) {
+          assert.equal(convertHandler.getUnit(32+ele),ele);
+        });
+        done();
+      });
+    test('Unknown Unit Input', function(done){
+      assert.equal(convertHandler.getUnit(32+'hello'),'invalid unit');
+      done();
+    });
+  }
 
+
+        
 });
